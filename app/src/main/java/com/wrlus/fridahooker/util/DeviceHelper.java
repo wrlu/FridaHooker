1a0925f5b84cf12de33b0f6c8ad74316f9cfa3c6
小路 <wrlus@outlook.com>
2019-10-23 14:51:33 +0800

    Remove requestRootPermission

--- app/src/main/java/com/wrlus/fridahooker/util/DeviceHelper.java
+++ app/src/main/java/com/wrlus/fridahooker/util/DeviceHelper.java
@@ -9,30 +9,6 @@ import java.io.InputStreamReader;
 
 public class DeviceHelper {
 
-    public static boolean requestRootPermission(String testCmd) {
-        try {
-            ProcessBuilder processBuilder = new ProcessBuilder("su");
-            processBuilder.redirectErrorStream(true);
-            Process process = processBuilder.start();
-            BufferedReader bs = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            DataOutputStream os = new DataOutputStream(process.getOutputStream());
-            os.writeBytes(testCmd + "\n");
-            os.writeBytes("exit\n");
-            os.flush();
-            process.waitFor();
-            String line;
-            while ((line = bs.readLine()) != null) {
-                Log.i("RootCheck", line);
-            }
-            if (process.exitValue() == 0) {
-                return true;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
     public static String[] getSupportedAbis() {
         return Build.SUPPORTED_ABIS;
     }
